1a84e25dde639c9eee4447c0b8e20693470399a2
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-22 19:14:59 +0200

    feat: Add Row for CabinTable

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,19 +1,19 @@
-import { useEffect } from 'react';
+import CabinTable from '../features/cabins/CabinTable';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apICabins';
 
 //FOR Testin in dev only
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
