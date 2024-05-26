4618d622d3dd7afc4f9a94608e22110a72f7d451
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-26 17:40:08 +0200

    feat: add sorting functionality with SortBy and Select components
    - Implemented  component:
      - Accepts  as props to configure sorting criteria.
      - Utilizes  to manage URL query parameters for sorting.
      - Determines the current sort parameter from URL or defaults to an empty string.
      - Provides  function to update the sort parameter and modify the URL.
      - Renders a  component for users to choose sorting options.
    
    - Added  component:
      - Accepts , , , and other props.
      - Renders a styled select element with provided options.
      - Maps through  to generate  elements for each sorting criterion.
    
    - Enhanced cabin filtering and sorting:
      - Integrated  component in the UI.
      - Added sorting options
      - Implemented filtering based on discount status:
        - All cabins
        - Cabins with no discount
        - Cabins with a discount
      - Implemented sorting logic:
        - Extracts  parameter from URL.
        - Splits the sorting parameter to determine field and direction.
        - Sorts the filtered cabins based on the selected criteria.

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -13,6 +13,7 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  // FILTER CABINS
   const filterValue = searchParams.get('discount') || 'all';
 
   let filteredCabins;
@@ -23,6 +24,14 @@ function CabinTable() {
 
   if (filterValue === 'with-discount')
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // SORT CABINS
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -36,7 +45,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
