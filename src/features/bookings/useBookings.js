6cab00947cf369b376a0a775f8968fd44fc928ab
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-30 11:02:36 +0200

    feat: add filtering functionality to bookings query
    Implemented filtering in the getBookings function:
    
    Applied filter to the query if filter parameters are provided.
    Supports different query methods (defaulting to 'eq') based on the filter.
    Updated useBookings hook:
    
    Utilizes URL search parameters to determine the filter value for booking status.
    Constructs a filter object if a specific status is selected, otherwise sets filter to null.
    Passes filter parameters to the getBookings function via the useQuery hook.

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,3 +1,4 @@
+import { useSearchParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
 import { getBookings } from '../../services/apiBookings';
@@ -7,13 +8,22 @@ import { getBookings } from '../../services/apiBookings';
  * @returns The `useBookings` function is returning an object with three properties: `isLoading`, `error`, and `bookings`. These properties are obtained from the `useQuery` hook and are related to fetching and managing booking data.
  */
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //FILTER
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, error, bookings };
