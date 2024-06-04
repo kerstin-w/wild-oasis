80ec5661b01918d98a0efa00c4acaf639d928e01
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-04 19:02:28 +0200

    feat: Implement useBooking hook to fetch booking details
    - Introduces useBooking hook for managing booking data with React Query.
    - Retrieves booking details based on ID from URL parameters (useParams).
    - Uses queryKey: ['booking'] to identify query and getBooking(bookingId) as query function.
    - Sets retry: false to disable automatic retries on errors (optional explanation).
    - Returns object containing isLoading, error, and booking data.
    - Integrates useBooking hook in component to access booking data.
    - Renders Spinner while data loads.
    - Extracts booking status and ID from fetched data (booking).
    
    This hook simplifies fetching and managing individual booking details.

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,22 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getBooking } from '../../services/apiBookings';
+
+/**
+ * The `useBooking` function uses a query to fetch booking data and returns loading status, error, and booking information.
+ * @returns The `useBooking` function is returning an object with three properties: `isLoading`, `error`, and `booking`. These properties are obtained from the `useQuery` hook and represent the loading state, any errors encountered, and the booking data fetched from the `getBooking` function, respectively.
+ */
+export function useBooking() {
+  const { bookingId } = useParams();
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ['booking'],
+    queryFn: () => getBooking(bookingId),
+    retry: false,
+  });
+
+  return { isLoading, error, booking };
+}
