881ef76b2f2feb5838d469161f5dd142b528972c
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-07 17:28:44 +0200

    feat: Add Logo, Heading and LoginForm component to Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import LoginForm from '../features/authentication/LoginForm';
+import Heading from '../ui/Heading';
+import Logo from '../ui/Logo';
 
 const LoginLayout = styled.main`
   min-height: 100vh;
@@ -10,8 +13,18 @@ const LoginLayout = styled.main`
   background-color: var(--color-grey-50);
 `;
 
+/**
+ * The Login function renders a login layout with a logo, heading, and login form in a React component.
+ * @returns The Login component is being returned, which includes a LoginLayout component with a Logo, a Heading component with the text "Log in to your account", and a LoginForm component.
+ */
 function Login() {
-  return <LoginLayout>Login</LoginLayout>;
+  return (
+    <LoginLayout>
+      <Logo />
+      <Heading as="h4">Log in to your account</Heading>
+      <LoginForm />
+    </LoginLayout>
+  );
 }
 
 export default Login;
