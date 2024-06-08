44aeaf58846e5103104f7a302493d6eaa3094b66
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-08 15:08:18 +0200

    docs: Add comment to useSignup hook

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -1,7 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
-import { signup as signupApi } from '../../services/apiAuth';
 import toast from 'react-hot-toast';
 
+import { signup as signupApi } from '../../services/apiAuth';
+
+/**
+ * The useSignup function handles user signup functionality, including making a signup API call and displaying a success message upon successful account creation.
+ * @returns The `useSignup` function is returning an object with two properties:
+ * 1. `signup`: This is a function that will trigger the signup process when called.
+ * 2. `isLoading`: This is a boolean value that indicates whether the signup process is currently loading or not.
+ */
 export function useSignup() {
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: signupApi,
