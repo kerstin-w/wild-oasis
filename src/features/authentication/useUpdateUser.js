651328b5ceca5b309a19ff41681b8819c0e1304a
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-08 16:17:14 +0200

    feat: Implement useUpdateUser hook and integrate with UpdateUserDataForm
    - Introduces useUpdateUser hook to manage user data update mutations and UI state.
    - Utilizes updateCurrentUser function for actual user data updates.
    - Handles successful updates with toast notification and query cache invalidation (user query).
    - Handles errors with toast notification displaying the error message.
    - Returns updateUser function for user data updates and isUpdating state for loading indication.
    
    - Integrates useUpdateUser hook in UpdateUserDataForm for user profile editing.
    - Manages form state with fullName (using currentFullName) and avatar (initially null).
    - Prevents form submission if fullName is empty.
    - Calls updateUser with fullName and possibly avatar on form submit.
    - Resets avatar state and form data in onSuccess callback of updateUser for clean UI state.
    - Implements handleCancel function to reset form state to initial values.
    
    This commit enables a dedicated hook for user profile updates and integrates it with UpdateUserDataForm for a user-friendly editing experience.

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
+import { updateCurrentUser } from '../../services/apiAuth';
+
+/**
+ * The `useUpdateUser` function is a custom hook that handles updating user data with mutation and provides loading state information.
+ * @returns An object is being returned with two properties:
+ * 1. `updateUser`: A function that can be used to update the current user.
+ * 2. `isUpdating`: A boolean value indicating whether the update operation is currently in progress.
+ */
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: () => {
+      toast.success('User successfully updated');
+      queryClient.invalidateQueries({ queryKey: ['user'] });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { updateUser, isUpdating };
+}
