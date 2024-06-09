0edf0328abe5b413936fedbcff8cb57643778083
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-09 12:44:40 +0200

    feat: Implement useRecentStays hook for confirmed stays management
    - Introduces useRecentStays hook for confirmed stays data management.
    - Utilizes built-in hook to access URL search parameters (retrieve specific parameter).
    - Sets default number of days (numDays) to 7 if specific parameter is missing.
    - Parses specific parameter value to a number if present in URL.
    - Calculates queryDate by subtracting numDays from current date (subDays function).
    - Leverages data fetching hook to retrieve stays after calculated queryDate.
    - Defines function for data fetching logic using another function.
    - Sets a unique identifier for the data fetch operation (queryKey with 'stays' and dynamic part).
    - Filters stays data to include only specific statuses (confirmed bookings).
    - Returns object containing loading state, fetched stays, and confirmed stays.
    
    This commit enables a reusable hook (useRecentStays) for fetching and managing confirmed stays data. It utilizes URL parameters for flexibility and leverages existing data fetching libraries for efficiency. Additionally, it filters the fetched stays to focus specifically on confirmed bookings.

---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
+import { subDays } from 'date-fns';
+import { useSearchParams } from 'react-router-dom';
+
+import { getStaysAfterDate } from '../../services/apiBookings';
+
+/**
+ * The `useRecentStays` function retrieves stays after a specified date and filters out stays with 'checked-in' or 'checked-out' status.
+ * @returns The `useRecentStays` function returns an object with three properties:
+ * 1. `isLoading`: A boolean indicating whether the data is currently loading.
+ * 2. `stays`: The array of stays fetched from the API based on the query date.
+ * 3. `confirmedStays`: A filtered array of stays that have a status of 'checked-in' or 'checked-out'.
+ */
+export function useRecentStays() {
+  const [searchParams] = useSearchParams();
+  const numDays = !searchParams.get('last')
+    ? 7
+    : Number(searchParams.get('last'));
+
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: stays } = useQuery({
+    queryFn: () => getStaysAfterDate(queryDate),
+    queryKey: ['stays', `last-${numDays}`],
+  });
+
+  const confirmedStays = stays?.filter(
+    (stay) => stay.status === 'checked-in' || stay.status === 'checked-out'
+  );
+
+  return { isLoading, stays, confirmedStays };
+}
