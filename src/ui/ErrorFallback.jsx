673bff91fa68a19b0fe48d6789dea0bdda06b51d
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-09 15:15:30 +0200

    feat: Implement ErrorFallback component for error handling
    - Introduces ErrorFallback component to display user-friendly error messages.
    - Receives error object and resetErrorBoundary function as props.
    
    - Renders GlobalStyles component for consistent styling.
    - Renders StyledErrorFallback component with:
      - Box container for organized layout.
      - Heading: Something went wrong.
      - Paragraph displaying the specific error message.
      - Button with Try Again label to trigger error boundary reset.
    
    This commit enables a reusable ErrorFallback component to gracefully handle unexpected errors. It displays clear and actionable error messages, allowing users to attempt recovery through a Try Again button.

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Heading from './Heading';
+import GlobalStyles from '../styles/GlobalStyles';
+import Button from './Button';
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -24,8 +27,30 @@ const Box = styled.div`
   }
 
   & p {
-    font-family: "Sono";
+    font-family: 'Sono';
     margin-bottom: 3.2rem;
     color: var(--color-grey-500);
   }
 `;
+
+/**
+ * The ErrorFallback component displays an error message and a button to try again in a React application.
+ * @returns The ErrorFallback component is being returned. It displays a message "Something went wrong", the error message, and a button to try again.
+ */
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <GlobalStyles>
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try Again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </GlobalStyles>
+  );
+}
+
+export default ErrorFallback;
