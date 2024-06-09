609ffce5f8961669d4b3cab4a9a7c6887f9e7084
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-09 14:58:15 +0200

    feat: Implement CheckoutButton component for checkout functionality
    - Introduces CheckoutButton component to initiate booking checkout.
    - Receives bookingId data as a prop.
    - Utilizes useCheckout hook for checkout functionality and loading state:
      - checkout function to trigger checkout process with bookingId.
      - isCheckingOut state to indicate ongoing checkout operation and disable button.
    
    - Renders Button component:
      - Primary variation for visual emphasis.
      - Small size for compact layout.
      - Triggers checkout function with bookingId upon click.
      - Disables button while checkout is in progress.
      - Displays Check out label.
    
    This commit enables a reusable CheckoutButton component for a user-friendly way to initiate booking checkout. It utilizes the useCheckout hook for secure and efficient checkout functionality.

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,19 @@
-import Button from "../../ui/Button";
+import Button from '../../ui/Button';
+import { useCheckout } from './useCheckout';
 
+/**
+ * The CheckoutButton component in a React application allows users to initiate the checkout process for a specific booking.
+ * @returns The `CheckoutButton` component is returning a button element with the text "Check out". The button has a primary variation, small size, and an onClick event handler that calls the `checkout` function with the `bookingId` as a parameter. The button is also disabled based on the `isCheckingOut` state value.
+ */
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingOut } = useCheckout();
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      onClick={() => checkout(bookingId)}
+      disabled={isCheckingOut}
+    >
       Check out
     </Button>
   );
