72bd1501774f6eeee2cc7c6c53f31af6f2a7dbd4
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-09 13:05:52 +0200

    feat: Implement Dashboard layout with loading state and statistics
    - Introduces comprehensive Dashboard layout component.
    - Utilizes multiple hooks to fetch and manage data:
      - Fetches bookings, confirmed stays, and days using specific hook.
      - Fetches cabin data and manages loading state with another hook.
    
    - Displays loading indicator (Spinner) while data retrieval is ongoing.
    
    - Renders StyledDashboardLayout component upon successful data retrieval.
    
    - Populates Stats component with fetched data:
      - Bookings data from specific hook.
      - Confirmed stays data (filtered from specific hook data).
      - Number of days from specific hook.
      - Cabin count derived from data length of another hook.
    
    This commit enables a dedicated Dashboard layout component that combines essential features:
      - Efficient data fetching using multiple hooks.
      - User-friendly loading indicator for data retrieval.
      - Clear and informative visualization of key statistics through the Stats component.

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+import Spinner from '../../ui/Spinner';
+import { useRecentBookings } from './useRecentBookings';
+import { useRecentStays } from './useRecentStays';
+import Stats from './Stats';
+import { useCabins } from '../cabins/useCabins';
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -8,9 +14,25 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoading2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Today's activity</div>
       <div>Chart stay durations</div>
       <div>Chart sales</div>
