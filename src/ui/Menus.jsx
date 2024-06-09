cec17a1636bbcb088cff4ddd551a21de9bf2314f
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-06-09 15:21:26 +0200

    fix: Toogling Menu not working. Add false to useOutsideClick and e.stopPropagation to click handler

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -94,6 +94,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const rect = e.target.closest('button').getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -116,7 +117,7 @@ function Toggle({ id }) {
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
 
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
   return createPortal(
